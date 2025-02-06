6045e9223694f689121cd1af54909de8d8eb9372
Евгений Кучма <desferto87@gmail.com>
2025-02-06 23:28:18 +0500

    lv

--- Program.cs
+++ Program.cs
@@ -6,67 +6,35 @@ namespace homeWorkPrentheticalEpression
     {
         static void Main(string[] args)
         {
-            string stringParentheticalesCorrect = "(()(()))";
- 
-            int[] numberesDeepParenthetical =  new int [1];
-            int[] tempNumbresDeepParenthetical = new int[numberesDeepParenthetical.Length + 1];
+            string parentheticalesCorrect = "(()(()))";
+             
+            int depth = 0;
+
+            int maxDepth = 0;   
 
             char openParenthetical = '(';
             char closeParenthetical = ')';
 
-            int countOpenParenthetical = 0;
-            int countCloseParenthetical = 0;
-            int maxDeepParenthetical = 0;
-
-            Console.Write(stringParentheticalesCorrect);
-            Console.WriteLine();
-
-            for (int i = 0; i < stringParentheticalesCorrect.Length; i++)
+            for (int i = 0; i < parentheticalesCorrect.Length; i++)
             {
-                if (stringParentheticalesCorrect[i] == openParenthetical)
+                if (parentheticalesCorrect[i] == openParenthetical)
                 {
-                    countOpenParenthetical++;
+                    depth++;
+                    maxDepth = Math.Max(maxDepth, depth);
                 }
-                else if (stringParentheticalesCorrect[i] == closeParenthetical)
+                else if (parentheticalesCorrect[i] == closeParenthetical)
                 {
-                    countCloseParenthetical++;
+                    depth--;
                 }
-                if((countOpenParenthetical - countCloseParenthetical) == 0)
+
+                if (depth < 0)
                 {
-                    numberesDeepParenthetical[0] = countOpenParenthetical;
-                    for (int j = 0; j < numberesDeepParenthetical.Length; j++)
-                    {
-                        tempNumbresDeepParenthetical[j] = numberesDeepParenthetical[j];
-                    }
-                    numberesDeepParenthetical = tempNumbresDeepParenthetical;
+                    Console.Write("Exention not correct");
+                    break;
                 }
             }
 
-            for (int i = 0; i < numberesDeepParenthetical.Length; i++)
-            {
-                Console.Write(numberesDeepParenthetical[i] + " ");
-            }
-
-            Console.WriteLine("\n\n\n");
-
-            if(countOpenParenthetical == countCloseParenthetical)
-            {
-                Console.WriteLine($"{stringParentheticalesCorrect} - its correct string parenthetical");
-            }
-            else
-            {
-                Console.WriteLine($"{stringParentheticalesCorrect} - its not correct string parenthetical");
-            }
-
-            for (int i = 0;i < numberesDeepParenthetical.Length - 1; i++)
-            {
-                if (numberesDeepParenthetical[i] > numberesDeepParenthetical[i + 1])
-                {
-                    maxDeepParenthetical = numberesDeepParenthetical[i];
-                }
-            }
-            
-            Console.WriteLine("Max deep = " + maxDeepParenthetical );
+            Console.WriteLine($"Exeption {parentheticalesCorrect} is correct, depth = {maxDepth}");
         }
     }
 }
